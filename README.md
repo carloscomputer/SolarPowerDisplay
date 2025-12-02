64f1a6405019b71dccbc7317b38b79f32ffe131f
carloscomputer <carlobster@icloud.com>
2025-12-02 13:30:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# A little solar power display
+# based on https://github.com/SonnenladenGmbH/APsystems-EZ1-API 
+# you'll need the "pip install apsystems-ez1"
---
+++ solar.power.display.py
@@ -0,0 +1,35 @@
+#written by Carlos Nikolaus Krämer aka carloscomputer https://github.com/carloscomputer
+import asyncio
+from APsystemsEZ1 import APsystemsEZ1M
+
+IP = "10.10.10.200"
+
+async def main():
+    dev = APsystemsEZ1M(IP)
+    out = await dev.get_output_data()
+
+    east = round(out.p1, 4)
+    south = round(out.p2, 4)
+    total = round(out.p1 + out.p2, 4)
+    east_d = round(out.e1, 4)
+    south_d = round(out.e2, 4)
+    total_d = round(out.e1 + out.e2, 4)
+    east_lt = round(out.te1, 4)
+    south_lt = round(out.te2, 4)
+    total_lt = round(out.te1 + out.te2,4)
+    
+    print("☀️ ACTUAL POWER☀️ ")
+    print("east:", out.p1, "W")
+    print("south:", out.p2, "W")
+    print("total", total, "W⚡️")
+    print("🌳 DAILY POWER 🌳")
+    print("east:", east_d, "kWh")
+    print("south:", south_d, "kWh")
+    print("total:", total_d, "kWh⚡️")
+    print("🌳☀️ LIFETIME POWER☀️🌳")
+    print("east:", east_lt, "kWh")
+    print("south:", south_lt, "kWh")
+    print("total:", total_lt, "kWh⚡️")
+
+asyncio.run(main())
+
